ef4a97901ed3ef6de259ec3aeef1ab6efa5f52db
aayushgupta15 <aayush61258@gmail.com>
2023-06-04 17:26:30 +0530

    style : footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import style from "../styles/modules/footer.module.css";
+import logo from "../images/logo.jpg";
+import { BsGithub, BsLinkedin, BsMailbox, BsTwitter } from "react-icons/bs";
+import { TfiEmail } from "react-icons/tfi";
 
 function Footer() {
   return (
-    <footer>
-      <div className="social">
+    <footer className={style.center}>
+      {/* company name */}
+      <div>
+        <img src={logo} alt="NA" />
+        <h4>FitQuest</h4>
+        <strong>Created by - Aayush Gupta</strong>
+      </div>
+
+      {/* social handles */}
+      <div className={style.social}>
         <ul>
           <li>
             <a href="#">
@@ -21,6 +32,10 @@ function Footer() {
               <BsGithub />
             </a>
           </li>
+          <a href="mailto:fitquest01@gmail.com">
+            <TfiEmail />
+          </a>
+          <li></li>
         </ul>
       </div>
     </footer>
