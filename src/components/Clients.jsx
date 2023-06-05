2de97988ba92958f2ae5370beafdcb85b0468d5e
aayushgupta15 <aayush61258@gmail.com>
2023-06-06 00:01:15 +0530

    images added for testimonial

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import style from "../styles/modules/services.module.css";
 import { getClasses } from "../utils/getClasses";
+import one from "../images/one.png";
+
+import two from "../images/two.png";
+import three from "../images/three.png";
 
 function Clients() {
   return (
@@ -8,23 +12,23 @@ function Clients() {
       <h2>What My Clients Say</h2>
       <hr />
       <div className={style.card__container}>
-        <div className={style.card}>
-          <img src="" alt="NA" />
+        <div className={getClasses([style.card, style.person])}>
+          <img src={one} alt="NA" />
           <p>
             FitQuest is the perfect fitness company for anyone looking to
             improve their overall health and wellness.
           </p>
         </div>
-        <div className={style.card}>
-          <img src="" alt="NA" />
+        <div className={getClasses([style.card, style.person])}>
+          <img src={two} alt="NA" />
           <p>
             FitQuest offers a wide range of services and programs that are both
             effective and enjoyable, making it easy to maintain a healthy
             lifestyle.
           </p>
         </div>
-        <div className={style.card}>
-          <img src="" alt="NA" />
+        <div className={getClasses([style.card, style.person])}>
+          <img src={three} alt="NA" />
           <p>
             The certified fitness professionals at FitQuest provided me with the
             guidance and motivation I needed to achieve my fitness goals.
